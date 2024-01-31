bd12c2fa4f5b6a9d1bf3d174b24ae7300754f281
utxra <92845083+utxra@users.noreply.github.com>
2024-01-31 11:47:45 +0100

    92873463278643276432784.php

--- 92873463278643276432784.php
+++ 92873463278643276432784.php
@@ -1,20 +1,24 @@
-<head>
-    <title>PHP 10</title>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" href="../../../favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="./index.css">
-</head>
-<?php
+<html>
+    
+    <head>
+        <title>PHP 10</title>
+        <meta charset="utf-8">
+        <link rel="shortcut icon" href="../../../favicon.png" type="image/x-icon">
+        <link rel="stylesheet" href="./index.css">
+    </head>
 
-$contraseñaftp = $_POST['contraseñaftp'];
-if ($contraseñaftp == '-1') {
-    echo "<p style=' position: fixed;
-    bottom: 0;
-    right: 0;
-    margin: 0;
-    padding: 10px;'>Clave: luke</p>";
-} else {
-    echo "Error";
-}
-?>
+    <?php
+    
+    $contraseñaftp = $_POST['contraseñaftp'];
+    if ($contraseñaftp == '-1') {
+        echo "<p style=' position: fixed;
+        bottom: 0;
+        right: 0;
+        margin: 0;
+        padding: 10px;'>Clave: luke</p>";
+    } else {
+        echo "Error";
+    }
+    ?>
+
 </html>
